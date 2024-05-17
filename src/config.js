a2ba9c33aca501492b6b68058ad389af2a355395
Gabriel <gabrieltorrealba09@gmail.com>
2024-05-17 13:02:11 -0430

    variables de entorno

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ export const PORT = process.env.PORT || 3000;
 
 export const database = {
     connectionLimit: 10,
-    host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "root",
-    password: process.env.DATABASE_PASSWORD || "2102",
-    database: process.env.DATABASE_NAME || "prueba",
-    port: process.env.DATABASE_PORT || 3306
+    host: process.env.MYSQLHOST || "localhost",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "2102",
+    database: process.env.MYSQLDATABASE || "prueba",
+    port: process.env.MYSQLPORT || 3306
 };
