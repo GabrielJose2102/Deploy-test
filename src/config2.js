299139666a2aaea12535c5c08d0b27182a79a7f6
Gabriel <gabrieltorrealba09@gmail.com>
2024-05-18 10:22:58 -0430

    Solo variables env necesarias

--- src/config2.js
+++ src/config2.js
@@ -7,6 +7,6 @@ export const database = {
     password: process.env.MYSQLPASSWORD || "2102",
     database: process.env.MYSQLDATABASE || "prueba",
     port: process.env.MYSQLPORT || 3306,
-    url: process.env.MYSQL_URL,
-    ulr_privada: process.env.MYSQL_PRIVATE_URL
+    /* url: process.env.MYSQL_URL,
+    ulr_privada: process.env.MYSQL_PRIVATE_URL */
 };
