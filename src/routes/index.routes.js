96daa3933c43b24586a9d6e4328376766704a458
Gabriel <gabrieltorrealba09@gmail.com>
2024-05-19 16:32:35 -0430

    Creacion de tablas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,10 +4,37 @@ import pool from '../database2.js';
 import {promisify} from 'util';
 
 /* const query = promisify(pool.query).bind(pool); */
+const consulta = `CREATE TABLE usuarios (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    fullname VARCHAR(255) NOT NULL
+);`
 
+const consulta2 = `SHOW TABLES;`
 
 router.get('/', (req, res)=>{
-    res.render('index.hbs')
+    pool.query(consulta2, (error, result) =>{
+        if (error) {
+            console.log('Error al consultar tablas');
+            return;
+        }
+        console.log(result[0]);
+
+        if (result[0] === undefined) {
+            pool.query(consulta, (error, result) =>{
+                if (error) {
+                    console.log('Error al crear tabla');
+                    return;
+                }
+                console.log(result[0]);
+                console.log('Creacion de Tablas exitosa');
+            })
+        } else {
+            console.log('Las tablas de la bd ya estan creadas');
+        }
+        res.render('index.hbs');
+    }) 
 });
 
 
@@ -21,7 +48,7 @@ router.get('/signin', (req, res)=>{
 
 
 router.get('/present', (req, res)=>{
-    pool.query('SELECT * FROM usuarios', (error, result, fields)=>{
+    pool.query('SELECT * FROM usuarios', (error, result)=>{
         if (error) {
             console.log('Error al consultar Tabla usuarios');
             return
